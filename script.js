1de457fcf67f3ff7436fb7d8bf91dade1ee2a12d
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-21 14:29:54 +0100

    Add leaflet library to display map

--- script.js
+++ script.js
@@ -15,6 +15,21 @@ navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    var map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
   },
   function () {
